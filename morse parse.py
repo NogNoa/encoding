b5d0d01036eea84272c22e1d187871cc70a42bbf
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-05-19 22:19:51 +0300

    paging and formatting

--- morse parse.py
+++ morse parse.py
@@ -5,36 +5,39 @@ Created on Sat Apr 25 07:50:08 2020
 @author: omer
 """
 
-Latin = {
-'.': {
-    ' ': 'e',
-    '.': {
-        ' ':'i',
-        '.': {
-            ' ': 's', '.':{' ': 'h'}, '-': {' ': 'v'}},
-        '-': {
-            ' ': 'u', '.': {' ': 'f'}}},
-    '-': {
-        ' ': 'a',
-        '.': {
-            ' ': 'r', '.': {' ': 'l'}},
-        '-': {
-            ' ': 'w', '.': {' ': 'p',}, '-': {' ': 'j'}}}},
-'-': {
-    ' ': 't',
-    '.': {
-        ' ': 'n',
-        '.': {
-            ' ': 'd', '.': {' ':'b'}, '-': {' ': 'x'}},
-        '-': {
-            ' ': 'k', '.': {' ': 'c'}, '-': {' ': 'y'}}},
-    '-': {
-        ' ': 'm',
-        '.': {
-            ' ': 'g', '.': {' ': 'z'}, '-': {' ': 'q'}},
-        '-': {' ': 'o'}}
-}}
+from types import MappingProxyType
 
+Latin = MappingProxyType({
+    '.': {
+        ' ': 'e',
+        '.': {
+            ' ': 'i',
+            '.': {
+                ' ': 's', '.': {' ': 'h'}, '-': {' ': 'v'}},
+            '-': {
+                ' ': 'u', '.': {' ': 'f'}}},
+        '-': {
+            ' ': 'a',
+            '.': {
+                ' ': 'r', '.': {' ': 'l'}},
+            '-': {
+                ' ': 'w', '.': {' ': 'p', }, '-': {' ': 'j'}}}},
+    '-': {
+        ' ': 't',
+        '.': {
+            ' ': 'n',
+            '.': {
+                ' ': 'd', '.': {' ': 'b'}, '-': {' ': 'x'}},
+            '-': {
+                ' ': 'k', '.': {' ': 'c'}, '-': {' ': 'y'}}},
+        '-': {
+            ' ': 'm',
+            '.': {
+                ' ': 'g', '.': {' ': 'z'}, '-': {' ': 'q'}},
+            '-': {' ': 'o'}}}})
+
+
+# noinspection GrazieInspection
 def tree_build(d: dict[str, str] | str):
     # take dict from char to morse
     # returns a tree from morse to char
@@ -43,9 +46,9 @@ def tree_build(d: dict[str, str] | str):
         return d
 
     back = {'.': {}, '-': {}}
-    for key,val in d.items():
+    for key, val in d.items():
         if not len(val):
-            # the SP branch if it exist is a key leaf
+            # the SP branch if it exists is a key leaf
             back[' '] = key
         else:
             # collect the items to a . branch and - branch
@@ -53,6 +56,8 @@ def tree_build(d: dict[str, str] | str):
     # construct the . and - branches. the SP branch returns immedietly
     return {m: tree_build(val) for m, val in back.items() if val}
 
+
+# noinspection GrazieInspection
 def tree_extend(tree: dict | str, d: dict[str, str] | str, overwrite: bool = False):
     if not d:
         return tree
@@ -61,11 +66,10 @@ def tree_extend(tree: dict | str, d: dict[str, str] | str, overwrite: bool = Fal
     elif isinstance(d, str) or isinstance(tree, str):
         return tree_build(d) if overwrite else tree
 
-
     back = {' ': {}, '.': {}, '-': {}}
-    for key,val in d.items():
+    for key, val in d.items():
         if not len(val):
-            # the SP branch if it exist is a key leaf
+            # the SP branch if it exists is a key leaf
             back[' '] = key
         else:
             # collect the items to a . branch and - branch
@@ -73,7 +77,7 @@ def tree_extend(tree: dict | str, d: dict[str, str] | str, overwrite: bool = Fal
     for m in ' .-':
         try:
             tree[m]
-        except (KeyError):
+        except KeyError:
             back[m] = tree_build(back[m])
         else:
             back[m] = tree_extend(tree[m], back[m], overwrite)
@@ -117,14 +121,15 @@ def MorseToBinary(morse):
             binary += '00'
     return binary
 
-def MorseToLatin(morse, tree=Latin):
+
+def MorseDecode(morse, tree=Latin):
     latin = ''
     morse += ' '
     while morse:
         c = tree
         while not isinstance(c, str):
             c = c[morse[0]]
-            morse =  morse[1:]
+            morse = morse[1:]
         latin += c
     return latin
 
@@ -133,6 +138,6 @@ if __name__ == '__main__':
     print(BinToMorse(0b111000100000001))
     # print(bin(137))
 
-    print(MorseToLatin('... --- ...'))
+    print(MorseDecode('... --- ...'))
     print(str(tree_build({"a": "-.", "b": "-...", "c": "-.-."})).replace("'},", "'},\n"))
     input()
